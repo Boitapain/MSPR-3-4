75eaf982701fdd962e79be620b0c5bc4c0ab349c
Vincent <vincentbullion@icloud.com>
2025-04-11 10:53:04 +0200

    11.04.2025 - CI/CD : modification api.py v4

--- app/api/api.py
+++ app/api/api.py
@@ -86,6 +86,8 @@ def update_diseases_route():
         return jsonify({"message": "Diseases updated successfully"}), 200
     except Exception as e:
         return jsonify({"message": str(e)}), 500
-
+    
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
+    app.run(host='0.0.0.0', port=port)
