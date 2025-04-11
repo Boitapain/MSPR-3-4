b8f0326ee1771a868a4e70e5378527632a98e91c
Vincent <vincentbullion@icloud.com>
2025-04-11 10:21:21 +0200

    11.04.2025 - CI/CD : correction v3 Backend requirements

--- app/api/api.py
+++ app/api/api.py
@@ -88,4 +88,4 @@ def update_diseases_route():
         return jsonify({"message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000)
