9c72a8972a46875749f5c9860bed836c8c008c0c
Vincent <vincentbullion@icloud.com>
2025-04-11 09:33:20 +0200

    11.04.2025 - CI/CD : modification api.py

--- app/api/api.py
+++ app/api/api.py
@@ -88,4 +88,4 @@ def update_diseases_route():
         return jsonify({"message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
