2e2dcf3ad171cbf2acf6b68cc37496dfe9a41b64
Vincent <vincentbullion@icloud.com>
2025-05-01 20:19:45 +0200

    01.05.2025 - Frontend : modifications navbar et statistiques

--- app/frontend/dashboard.py
+++ app/frontend/dashboard.py
@@ -10,61 +10,45 @@ def dashboard(user):
     if 'dashboard_page' not in st.session_state:
         st.session_state['dashboard_page'] = 'home'
 
-    # Custom CSS for styling
-    st.markdown("""
-    <style>
-        h1 {
-            text-shadow: 2px 2px 6px rgba(168, 168, 168, 1);
+    # CSS
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"] {
+            background-color: #D1D5DB !important;
+            width: fit-content !important;
+
+        [data-testid="stSidebar"] span {
+            color: #6EE7B7 !important;
         }
-        [data-testid=stSidebar] {
-            background-color: #6EE7B7;
-        }
-        .stButton button{
-                display: flex;  
-                margin: 10px auto;
-        }
-        .stButton button:hover{
-                color:black;
-                transition: 0.5s;
-                transform: scale(1.1);
-        }
-        .stButton button span {
-            font-size: 2.5rem;
-            margin-right: 20px;
-            text-align: center;
-            text-shadow: 2px 2px 6px rgba(100, 100, 100, 1);
-        }
-        .stButton button div {
-            font-size: 1.25rem;
-            text-align: center;
-        }
-    </style>
-    """, unsafe_allow_html=True)
-    
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     # Sidebar with navigation buttons
     with st.sidebar:
         # Home button
         st.button("Home", type="tertiary", icon=":material/home:", on_click=lambda: st.session_state.update({"dashboard_page": "home"}))
-        
-        st.divider()
-        
+        st.markdown("#")
+
         # CSV Import button
+        st.header("Data Management")
         if user["isAdmin"]:
             st.button("CSV Import", type="tertiary", icon=":material/download:", on_click=lambda: st.session_state.update({"dashboard_page": "csv_import"}))
-            
-            st.divider()
         
         # Database Visualization button
         st.button("Database", type="tertiary", icon=":material/database:", on_click=lambda: st.session_state.update({"dashboard_page": "database"}))
-
-        st.divider()
+        st.markdown("#")
         
+        st.header("Data Visualization")
         # Database Visualization button
         st.button("Statistics", type="tertiary", icon=":material/monitoring:", on_click=lambda: st.session_state.update({"dashboard_page": "statistics"}))
+        st.markdown("#")
         
-        st.divider()
         
         # Settings button
+        st.header("Settings")
         st.button("Profile", type="tertiary", icon=":material/person:", on_click=lambda: st.session_state.update({"dashboard_page": "profile"}))
     
     # Display the appropriate page based on session state
--- app/frontend/dashboard_pages/profile.py
+++ app/frontend/dashboard_pages/profile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def profile(user):
-    st.markdown("<h1 style='text-align: center;'>You profile</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Your profile</h1>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size:1.5rem;'>User: <b>{user['name']}</b></p>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size:1.5rem;'>Email: <span style='color:blue;'>{user['email']}</span></p>", unsafe_allow_html=True)
     st.markdown(f"<p style='font-size:1.5rem;'>Is Admin: <b>{True if user['isAdmin'] == 1 else False}</b></h3>", unsafe_allow_html=True)
