d9159d86db4b6fdd0a95a379c1f3b692431521cc
Vincent <vincentbullion@icloud.com>
2025-05-27 21:54:17 +0200

    27.05.2025 - Notebook : réparation des données

--- app/notebook/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start MLflow server in the background
-mlflow server --host 0.0.0.0 --port 8008 --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns &
-
-# Start Jupyter Notebook in the foreground
-exec jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --no-browser
